258a19f2ea7869a5d987cd431fa486469377ed25
itang3141 <itang2@illinois.edu>
2024-10-06 17:18:23 -0500

    Added docker-compose and Dockerfiles for Java/Springboot and Python

---
+++ backend/src/test/java/TeleSxope/coursinator/ApplicationTests.java
@@ -0,0 +1,13 @@
+package TeleSxope.coursinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
