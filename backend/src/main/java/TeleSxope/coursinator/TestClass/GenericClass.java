1b279b668cb95097bccca8a5af2def2c330f09aa
ITHongHao <ivantang3141@gmail.com>
2024-10-13 16:03:13 -0500

    Dockerfile change to add MySQL database and other minor changes

---
+++ backend/src/main/java/TeleSxope/coursinator/TestClass/GenericClass.java
@@ -0,0 +1,72 @@
+package TeleSxope.coursinator.TestClass;
+
+import java.time.LocalDate;
+
+public class GenericClass {
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private Integer age;
+
+    public GenericClass() {
+
+    }
+
+    public GenericClass(Long id, String name, String email, LocalDate dob, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public GenericClass(String name, String email, LocalDate dob, Integer age) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public LocalDate getDob() {
+        return this.dob;
+    }
+
+    public Integer getAge() {
+        return this.age;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+
+}
