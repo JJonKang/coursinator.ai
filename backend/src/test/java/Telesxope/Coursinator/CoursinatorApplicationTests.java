4e8aa1387195074568d5d44be4267ae5334f51c3
“itang3141” <itang2@illinois.edu>
2024-12-16 01:16:48 -0600

    Changing structure to Java backend and React/Vite frontend

---
+++ backend/src/test/java/Telesxope/Coursinator/CoursinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package Telesxope.Coursinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursinatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
